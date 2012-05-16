0e33802f5b504f9b3199531892a8a0d0fb538c33
Tobias Verhoeven <t.s.verhoeven@gmail.com>
2012-05-16 18:44:28 +0200

    - Added extension point for ResourceFactories - Removed MongoDB resource

--- org.eclipse.emf.emfstore.common/src/org/eclipse/emf/emfstore/common/ResourceFactoryRegistry.java
+++ org.eclipse.emf.emfstore.common/src/org/eclipse/emf/emfstore/common/ResourceFactoryRegistry.java
@@ -11,30 +11,43 @@
 package org.eclipse.emf.emfstore.common;
 
 import java.util.Map;
-
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.Resource.Factory;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.emfstore.common.extensionpoint.ExtensionElement;
+import org.eclipse.emf.emfstore.common.extensionpoint.ExtensionPoint;
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
 
 /**
  * 
  * @author emueller
  */
 public class ResourceFactoryRegistry extends XMIResourceFactoryImpl implements Resource.Factory.Registry {
-
+	
+	private ResourceFactoryImpl factory=null;
+	
 	@Override
 	public Resource createResource(URI uri) {
-		//return new EMFStoreResource(uri);
-		return new MongoDBResource(uri);
+		return new EMFStoreResource(uri);
 	}
-
+	
+	
 	public Factory getFactory(URI uri) {
-		return this;
+		if (this.factory == null) {
+			ExtensionElement element = new ExtensionPoint("org.eclipse.emf.emfstore.common.resourcefactory").getElementWithHighestPriority();
+			if (element != null) {
+				this.factory = element.getClass("class", ResourceFactoryImpl.class);
+			}
+			if (this.factory == null) {
+				this.factory = this;
+			}
+		}
+		return this.factory;
 	}
 
 	public Factory getFactory(URI uri, String contentType) {
-		return this;
+		return this.getFactory(null);
 	}
 
 	public Map<String, Object> getProtocolToFactoryMap() {
