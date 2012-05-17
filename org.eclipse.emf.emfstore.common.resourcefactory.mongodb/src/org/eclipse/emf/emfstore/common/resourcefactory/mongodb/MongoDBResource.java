643378fd69dc0c5f383abeaff170605720f684e5
Tobias Verhoeven <t.s.verhoeven@gmail.com>
2012-05-17 20:50:58 +0200

    - projects are no longer saved in individual collections - complete fileUri as id in order to avoid server and client conflicts

--- org.eclipse.emf.emfstore.common.resourcefactory.mongodb/src/org/eclipse/emf/emfstore/common/resourcefactory/mongodb/MongoDBResource.java
+++ org.eclipse.emf.emfstore.common.resourcefactory.mongodb/src/org/eclipse/emf/emfstore/common/resourcefactory/mongodb/MongoDBResource.java
@@ -1,19 +1,32 @@
 package org.eclipse.emf.emfstore.common.resourcefactory.mongodb;
 
+import java.util.HashMap;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.eclipse.emf.emfstore.common.EMFStoreResource;
 import org.eclipselabs.mongo.emf.MongoURIHandlerImpl;
 
 public class MongoDBResource extends XMIResourceImpl {
 
 	private static org.eclipse.emf.ecore.resource.URIHandler uriHandler;;
 
-	public MongoDBResource(URI uri) {
-		super(URI.createURI("mongo://127.0.0.1:27017/emf/" + uri.segment(uri.segmentCount() - 2) + "/"
-			+ uri.lastSegment()));
-		System.out.println("URI: " + uri);
+	public MongoDBResource(URI uri_) {
+		super(mongoDbUriFromEmfUri(uri_));
+	
+		System.out.println("URI-: " + uri_);
+	}
+	
+	private static URI mongoDbUriFromEmfUri(URI uri) {
+		if (uri.toString().startsWith("mongo")) {
+			return uri;
+		}
+	
+		return URI.createURI("mongo://127.0.0.1:27017/emf/objects/"
+			+ uri.devicePath().replace("/", "-"));
 	}
 
 	@Override
